2773d9b77c9e0c1e206a59ddca0a980f86cd4e95
LoickMarion <loick.marion@gmail.com>
2025-03-22 12:35:00 -0400

    fds

--- analysis/interpolation_experiment/loick-2D-sweep.py
+++ analysis/interpolation_experiment/loick-2D-sweep.py
@@ -76,64 +76,64 @@ def run_8_short_reflector(n_si, spec_r):
             )    
     pte = am.photon_transport_efficiency
     
-    def run_4_tall_reflector(n_si, spec_r):
-        experiment_name = "8Silicon35_87"
-        num_particles = 2_000_000
-        seed = 1042
-        plots = []
-        exclusion = []
-        excluded = [f"reflector{i}" for i in exclusion]
-        k_si = 0.5
-        
-        ptes = []
-        ptes_err = []
-        mm = MaterialManager(experiment_name) 
+def run_4_tall_reflector(n_si, spec_r):
+    experiment_name = "8Silicon35_87"
+    num_particles = 2_000_000
+    seed = 1042
+    plots = []
+    exclusion = []
+    excluded = [f"reflector{i}" for i in exclusion]
+    k_si = 0.5
+    
+    ptes = []
+    ptes_err = []
+    mm = MaterialManager(experiment_name) 
 
-        mm.add_attributes(mm.materials["silicon"], refractive_index=n_si)
-        mm.material_props["silicon"]["eta"] = n_si
-        mm.material_props["silicon"]["k"] = k_si
-        sm = SurfaceManager(mm, experiment_name) 
-        sm.overwrite_property("silicon-Xe", "reflect_specular", spec_r)
-        sm.overwrite_property("silicon-Xe", "reflect_diffuse", 1-spec_r)
+    mm.add_attributes(mm.materials["silicon"], refractive_index=n_si)
+    mm.material_props["silicon"]["eta"] = n_si
+    mm.material_props["silicon"]["k"] = k_si
+    sm = SurfaceManager(mm, experiment_name) 
+    sm.overwrite_property("silicon-Xe", "reflect_specular", spec_r)
+    sm.overwrite_property("silicon-Xe", "reflect_diffuse", 1-spec_r)
 
-        gm = GeometryManager(
-            experiment_name=experiment_name,
-            surf_manager=sm,
-            exclude=excluded
-        )
-        fail_counter = 0
-        
-        rm = None
-        while fail_counter < 3: #handle rare CUDA error
-            try:
-                rm = RunManager(
-                    geometry_manager=gm,
-                    random_seed=seed,
-                    num_particles=num_particles,
-                )
-            except Exception as e:
-                fail_counter += 1
-                if fail_counter == 3:
-                    print("failed 3 times")
-                    print(e)
-                    raise e
-                    exit()
-                continue
-            break
+    gm = GeometryManager(
+        experiment_name=experiment_name,
+        surf_manager=sm,
+        exclude=excluded
+    )
+    fail_counter = 0
+    
+    rm = None
+    while fail_counter < 3: #handle rare CUDA error
+        try:
+            rm = RunManager(
+                geometry_manager=gm,
+                random_seed=seed,
+                num_particles=num_particles,
+            )
+        except Exception as e:
+            fail_counter += 1
+            if fail_counter == 3:
+                print("failed 3 times")
+                print(e)
+                raise e
+                exit()
+            continue
+        break
 
-        photons, photon_tracks, particle_histories = rm.get_simulation_results()
-        am = AnalysisManager(
-                    gm,
-                    experiment_name,
-                    plots,
-                    photons,
-                    photon_tracks,
-                    seed,
-                    particle_histories,
-                    save = False,
-                    show = False,
-                    print = False,
-                )    
-        pte = am.photon_transport_efficiency
+    photons, photon_tracks, particle_histories = rm.get_simulation_results()
+    am = AnalysisManager(
+                gm,
+                experiment_name,
+                plots,
+                photons,
+                photon_tracks,
+                seed,
+                particle_histories,
+                save = False,
+                show = False,
+                print = False,
+            )    
+    pte = am.photon_transport_efficiency
 
-        return pte
+    return pte
